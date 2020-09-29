f9de49551ca833b3e3a31b495ef84de5503f4ebf
akjha96 <akjha96@gmail.com>
2020-09-29 23:12:29 +0530

    Updates

---
+++ precache-manifest.9a79d2b46747ac5169a2927b5418f8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eabf091ac8105ccddeeeb7723eedeef",
+    "url": "/Todo-List-React/index.html"
+  },
+  {
+    "revision": "22f30a4a410b49e645fa",
+    "url": "/Todo-List-React/static/css/main.b7ca230a.chunk.css"
+  },
+  {
+    "revision": "cfabaf34f40d29d47d10",
+    "url": "/Todo-List-React/static/js/2.fd6010db.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-List-React/static/js/2.fd6010db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f30a4a410b49e645fa",
+    "url": "/Todo-List-React/static/js/main.12238e7e.chunk.js"
+  },
+  {
+    "revision": "337def37d37b3919b4e6",
+    "url": "/Todo-List-React/static/js/runtime-main.2c06ab1e.js"
+  },
+  {
+    "revision": "04fabb58d62639e108f1849a240949cc",
+    "url": "/Todo-List-React/static/media/bg.04fabb58.webp"
+  }
+]);
